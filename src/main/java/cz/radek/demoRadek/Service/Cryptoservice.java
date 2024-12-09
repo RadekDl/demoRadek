7257e6f1360e19c939292ce59b04ed7187007fad
RadekDl <radek.dl@atlas.cz>
2024-12-09 22:07:10 +0100

    zčátek projektu 2
    nachystání crypta, Cryptoservice, CryptoController DemoRadekApplication
    řazení dle mappingu
    oprava servise a controlleru

--- src/main/java/cz/radek/demoRadek/Service/Cryptoservice.java
+++ src/main/java/cz/radek/demoRadek/Service/Cryptoservice.java
@@ -1,9 +1,42 @@
 package cz.radek.demoRadek.Service;
 
+import cz.radek.demoRadek.Model.Crypto;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
 public class Cryptoservice {
+
+    private List<Crypto> cryptoList = new ArrayList<>();
+
+    public void addCrypto(Crypto crypto){
+        cryptoList.add(crypto);
+    }
+    public List<Crypto> getCryptoList(){
+        return cryptoList;
+    }
+    public int getCryptoListName() {
+        cryptoList.sort(Comparator.comparing(Crypto::getName));
+        return cryptoList.size();
+    }
+    public List<Crypto> sortingPrice() {
+        cryptoList.sort(Comparator.comparing(Crypto::getPrice));
+        return cryptoList;
+    }
+    public List<Crypto> sortingQuantity() {
+        cryptoList.sort(Comparator.comparing(Crypto::getQuantity));
+        return cryptoList;
+    }
+    public void  InformationById(int id){
+
+        System.out.println(cryptoList.get(id).getName()+cryptoList.get(id).getPrice()+cryptoList.get(id).getSymbol()
+                +cryptoList.get(id).getQuantity());
+    }
+
+
 }
--- src/main/java/cz/radek/demoRadek/controller/CryptoController.java
+++ src/main/java/cz/radek/demoRadek/controller/CryptoController.java
@@ -1,6 +1,8 @@
 package cz.radek.demoRadek.controller;
 
 import cz.radek.demoRadek.Model.Crypto;
+import cz.radek.demoRadek.Service.Cryptoservice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,43 +12,48 @@ import java.util.Comparator;
 import java.util.List;
 
 @RestController
-@RequestMapping ("api/list")
+//@RequestMapping ("api/list")
+@RequestMapping("crypto")
 public class CryptoController {
 
-    private List<Crypto> cryptoList = new ArrayList<>();
+//    private List<Crypto> cryptoList = new ArrayList<>();
+//
+//
+//    @GetMapping
+//    public List<Crypto> getCryptoList() {
+//        return cryptoList;
+//    }
+//
+//    @GetMapping("/size")
+//    public int getCryptoListSize() {
+//        cryptoList.sort(Comparator.comparing(Crypto::getName));
+//        return cryptoList.size();
+//    }
+//
+//    // přidání do listu crypto
+//    @PostMapping("/add")
+//    public ResponseEntity<String> addCrypto(@RequestBody Crypto crypto) {
+//        cryptoList.add(crypto);
+//        return ResponseEntity.status(HttpStatus.CREATED).body("Crypto added successfully");
+//    }
+//
+//
+//    @GetMapping("/price")
+//    public List<Crypto> sortingPrice() {
+//        // Seřazení crypta cena
+//        cryptoList.sort(Comparator.comparing(Crypto::getPrice));
+//        return cryptoList;
+//    }
+//
+//    @GetMapping("/quantity")
+//    public List<Crypto> sortingQuantity() {
+//        // Seřazení crypta počet jednotek
+//        cryptoList.sort(Comparator.comparing(Crypto::getQuantity));
+//        return cryptoList;
+//    }
+    @Autowired
+    private Cryptoservice cryptoservice;
 
 
-    @GetMapping
-    public List<Crypto> getCryptoList() {
-        return cryptoList;
-    }
-
-    @GetMapping("/size")
-    public int getCryptoListSize() {
-        cryptoList.sort(Comparator.comparing(Crypto::getName));
-        return cryptoList.size();
-    }
-
-    // přidání do listu crypto
-    @PostMapping("/add")
-    public ResponseEntity<String> addCrypto(@RequestBody Crypto crypto) {
-        cryptoList.add(crypto);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Crypto added successfully");
-    }
-
-
-    @GetMapping("/price")
-    public List<Crypto> sortingPrice() {
-        // Seřazení crypta cena
-        cryptoList.sort(Comparator.comparing(Crypto::getPrice));
-        return cryptoList;
-    }
-
-    @GetMapping("/quantity")
-    public List<Crypto> sortingQuantity() {
-        // Seřazení crypta počet jednotek
-        cryptoList.sort(Comparator.comparing(Crypto::getQuantity));
-        return cryptoList;
-    }
 }
 
